65f7a6c7ca7b6968edeea73b4c078dc133c9ab5c
Laurenz Bilek <e11904655@student.tuwien.ac.at>
2025-10-18 12:07:54 +0200

    chore: adapt naming conventions for main application class

--- backend/src/main/java/at/sykios/communityEventTool/CommunityEventToolApplication.java
+++ backend/src/main/java/at/sykios/communityEventTool/CommunityEventToolApplication.java
@@ -1,13 +1,13 @@
-package com.example.communityevent;
+package at.sykios.communityEventTool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommunityEventApplication {
+public class CommunityEventToolApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommunityEventApplication.class, args);
+        SpringApplication.run(CommunityEventToolApplication.class, args);
     }
 
 }
